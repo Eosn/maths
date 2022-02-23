b3281825ba0553f0d7269da2ae26163f32ca6f16
Éllen Neves <ellenosneves@gmail.com>
2022-02-22 22:04:58 -0300

    Create tabuada.py

---
+++ tabuada.py
@@ -0,0 +1,9 @@
+def f_Tabuada():
+    i = 0
+    while i <= 10:
+        j = 0
+        while j <= 10:
+            print(i, " x ", j, " = ", i*j)
+            j += 1
+        i += 1
+        print("")
